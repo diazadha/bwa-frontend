c6b0d244a239924ac2f59e996786560c8b207ba2
Diaz Adha Asri Prakoso <76459391+diazadha@users.noreply.github.com>
2022-10-20 14:48:36 +0700

    chore(tailwind): add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/index.js"],
+  content: ["./pages/**/*"],
   presets: [],
   darkMode: "media", // or 'class'
   theme: {
