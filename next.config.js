cf36bff540b98c1faaf9410e447b41d10cac8894
Diaz Adha Asri Prakoso <76459391+diazadha@users.noreply.github.com>
2022-10-20 08:28:13 +0700

    first setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,22 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const withPlugins = require("next-compose-plugins");
+const withCss = require("@zeit/next-css");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
+const path = require("path");
+
+(module.exports = nextConfig),
+  withPlugins([
+    withCss({}),
+    withImages({}),
+    withReactSvg({
+      include: path.resolve(__dirname, "./public/images"),
+      webpack(config, options) {
+        return config;
+      },
+    }),
+  ]);
